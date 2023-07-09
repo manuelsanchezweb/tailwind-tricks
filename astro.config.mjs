0b4426484bbed0446b4fb42780d83e0040f1d806
manuelsanchez2 <manusansan22@gmail.com>
2023-07-09 20:04:50 +0200

    Initial Commit 🍌

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
+  integrations: [tailwind()]
+});
